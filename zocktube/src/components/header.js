e17192fba33d5a586e3a2fb8ae238ac5eab679b6
Mehmet Ozdemir <mcozdemi@buffalo.edu>
2019-12-11 21:18:24 -0500

    Anime submittion

--- zocktube/src/components/header.js
+++ zocktube/src/components/header.js
@@ -17,10 +17,11 @@ const Header = ({ siteTitle, callBackAnime }) => {
   <header className="header">
     <div className="container header-content">
       <nav className="navbar">
-        <a className="navbar-brand" href="/">{siteTitle}</a>
+        <a className="navbar-brand" href="/"><h1>{siteTitle}</h1></a>
         <form className="form-inline">
           <input onChange={(e) => setAnime(e.target.value)} value={anime} className="form-control" id="animeSearch" type="text" placeholder="Enter Anime URL" aria-label="Enter Anime URL"/>
-          <button onClick={handleSubmit} className="btn text-white btn-outline-dark" type="button">Enter</button>
+          <button onClick={handleSubmit} className=" button-left-space btn text-white btn-outline-dark" type="button">Enter</button>
+          <a className="text-white btn btn-outline-info button-left-space" target="_blank" href="https://www.animebam.se/series">Find Anime Here!</a>
         </form>
       </nav>
     </div>
